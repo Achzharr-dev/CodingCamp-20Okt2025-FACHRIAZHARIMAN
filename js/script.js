ebdfd6666314cc8b82a26c74b1aafbd41bcff41c
Achzharr-dev <kucingkalem21@gmail.com>
2025-10-24 20:23:25 +0700

    add js & css to HTML

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
+
 function welcomespeech() {
     let name = prompt("Please enter your name:");
 
-    document.getElementById("welcome-message").innerText = 'HI ${name}, ';
+    document.getElementById("welcome-message").innerHTML = `HI ${name}, `;
 
+}
+
+function validateForm() {
+    const name = document.getElementById('name-input').value;
+    const email = document.getElementById('email-input').value;
+    const message = document.getElementById('message-input').value;
+
+    if (!name === ""|| email === ""|| message === "") {
+        alert("Please fill in all fields.");
+    } else {
+        alert(`Thank you! ${name}, your message has been sent successfully!`);
+    }
 }
